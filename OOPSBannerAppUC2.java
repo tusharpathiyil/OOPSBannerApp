087b1039d86fe6da2aa2c8de523a35df30886d02
tusharpathiyil <tusharpathiyil@gmail.com>
2026-02-14 15:20:52 +0530

    UC2: Print OOPS ASCII banner

---
+++ OOPSBannerAppUC2.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerAppUC2
+ * Prints OOPS in ASCII banner
+ *
+ * @author Tushar Pathiyil
+ * @version 2.0
+ */
+
+public class OOPSBannerAppUC2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println(" *****   *****  *       ***** ");
+
+    }
+}
