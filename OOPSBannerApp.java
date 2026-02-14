39df3598fc61649ad69145e4cac051fa3db71da6
tusharpathiyil <tusharpathiyil@gmail.com>
2026-02-14 13:20:44 +0530

    UC1: Print OOPS banner to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,22 @@
+/**
+ * OOPSBannerApp
+ * Prints OOPS to console
+ *
+ * @author Tushar Pathiyil
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****   *****   *****   *****");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *     * *     ");
+        System.out.println("*     * *     * *****   ***** ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println("*     * *     * *           * ");
+        System.out.println(" *****   *****  *       ***** ");
+
+    }
+}
