947f1ae2be265e79a1a51b3a8b334e0295c4cfc4
tusharpathiyil <tusharpathiyil@gmail.com>
2026-03-04 09:51:46 +0530

    UC8: Implement Map Collection and Render Function for OOPS Banner

---
+++ OOPSBannerAppUC8.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class OOPSBannerAppUC8 {
+
+    // Centralized Map for storing character patterns
+    private static final Map<Character, String[]> patternMap = new HashMap<>();
+
+    // Static block to initialize patterns
+    static {
+        patternMap.put('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        });
+
+        patternMap.put('P', new String[]{
+                "****** ",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      "
+        });
+
+        patternMap.put('S', new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        });
+    }
+
+    // Render function (separation of display logic)
+    public static void renderBanner(String word) {
+
+        int height = 5;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[i]).append("   ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String word = "OOPS";
+
+        renderBanner(word);
+    }
+}
