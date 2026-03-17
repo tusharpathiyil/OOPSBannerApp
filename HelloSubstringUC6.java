4e8d077ca71a71a74484bc9f10cae554cc2f2e14
tusharpathiyil <tusharpathiyil@gmail.com>
2026-03-17 08:46:39 +0530

    UC6: Display Hello using substring to remove trailing delimiter

---
+++ HelloSubstringUC6.java
@@ -0,0 +1,24 @@
+public class HelloSubstringUC6 {
+
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+
+            String names = "";
+
+            for (String name : args) {
+                names += name + ", ";
+            }
+
+            // Remove trailing comma and space
+            names = names.substring(0, names.length() - 2);
+
+            System.out.println("Hello " + names);
+
+        } else {
+            System.out.println("Hello, World!");
+        }
+
+    }
+
+}
